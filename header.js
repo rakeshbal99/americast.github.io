5d27a3d8ba0fe3b1f7a113da1049b16a082290b4
americast <sayan.sinha@iitkgp.ac.in>
2018-04-29 03:22:44 +0530

    place blog inside folder

--- header.js
+++ header.js
@@ -8,20 +8,20 @@ document.write('\
                     <span class="sr-only">Toggle navigation</span>\
                     Menu <i class="fa fa-bars"></i>\
                 </button>\
-                <a class="navbar-brand" href="index.html">Sayan Sinha</a>\
+                <a class="navbar-brand" href="/index.html">Sayan Sinha</a>\
             </div>\
-\
+            \
             <!-- Collect the nav links, forms, and other content for toggling -->\
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">\
                 <ul class="nav navbar-nav navbar-right">\
                     <li>\
-                        <a href="index.html">Home</a>\
+                        <a href="/index.html">Home</a>\
                     </li>\
                     <li>\
-                        <a href="blog.html">Blog</a>\
+                        <a href="/blog">Blog</a>\
                     </li>\
                     <li>\
-                        <a href="contact.html">Contact</a>\
+                        <a href="/contact.html">Contact</a>\
                     </li>\
                 </ul>\
             </div>\
